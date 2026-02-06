49a08b849f9e660926da53ed1176fce45333c1ea
Adriel Windows <adrielcostamartins608@gmail.com>
2026-02-06 00:05:50 -0300

    ajustes

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function confirmacao(botao){
         alert('notificação enviada')
         botao.style.backgroundColor = 'yellow'
     }
+
+function confirmacao(botao) {
+    const confirmar = confirm('Deseja confirmar o envio da nota?')
+
+    if (confirmar) {
+        botao.style.display = 'none'
+    }
+}
+
